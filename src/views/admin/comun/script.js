fed4f1f581f3994a286a8c97573b721f8dae0ac5
Felipe Urrutia <thewomins@gmail.com>
2022-06-12 11:18:22 -0400

    added views

---
+++ src/views/admin/comun/script.js
@@ -0,0 +1,10 @@
+import {template, funcionalidad_menu} from "../../../Components/menu_lateral_admin/menu.js";
+
+
+let menu = document.getElementById("menu");
+menu.innerHTML = template
+funcionalidad_menu()
+console.log(template)
+
+
+//menu.innerHTML = 
