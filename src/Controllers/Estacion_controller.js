9291dd11e5d289aa1b082b6c8dde2b61fcbfc4a6
Felipe Urrutia <thewomins@gmail.com>
2022-06-16 11:11:08 -0400

    added controller to estacion and functionality

--- src/Controllers/Estacion_controller.js
+++ src/Controllers/Estacion_controller.js
@@ -1,5 +1,6 @@
 import {Estacion} from "../Models/Estacion.js"
 
+
 const endpoint_estacion = "http://127.0.0.1:8000/estaciones";
 
 /*
@@ -11,6 +12,28 @@ fetch("/post/data/here", {
     console.log("Request complete! response:", res);
   });
 */
+console.log("ingreso")
 
 const request = await fetch(endpoint_estacion);
 console.log(request.json())
+
+function add_estacion(nombre_estacion,nombre_ciudad){
+  let estacion = new Estacion(nombre_estacion,nombre_ciudad)
+  fetch(endpoint_estacion+"/post", {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(estacion),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+export {add_estacion};
+
--- src/views/admin/scripts/estaciones.js
+++ src/views/admin/scripts/estaciones.js
@@ -1,3 +1,14 @@
-import * as lala from "../../../Controllers/Estacion_controller.js" ;
+import {add_estacion} from "../../../Controllers/Estacion_controller.js" ;
 
-lala
+document.getElementById("boton-post").addEventListener("click",()=>{post_estacion()});
+
+function post_estacion(){
+    console.log("click")
+    let nombre_estacion = document.getElementById("Nombre_estacion").value
+    let ciudad_estacion = document.getElementById("Ciudad_estacion").value
+    if(nombre_estacion ==="" || ciudad_estacion===""){return}
+    console.log(nombre_estacion)
+    console.log(ciudad_estacion)
+    add_estacion(nombre_estacion,ciudad_estacion)
+}
+
