bd628acbdd32519606e2d3cc13ebd2ad6f1b7a65
Felipe Urrutia <thewomins@gmail.com>
2022-06-20 21:08:22 -0400

    templates deleted from comun

--- src/views/admin/comun/script.js
+++ src/views/admin/comun/script.js
@@ -1,43 +1,6 @@
 import {template,funcionalidad_menu,} from "../../../Components/menu_lateral_admin/menu.js";
 
-const template_add_estaciones = `
-<div class="input-field-fill margin-1">
-    <input type="input" class="form__field" placeholder=" " name="Nombre_estacion" id="Nombre_estacion"
-        required />
-    <label for="Nombre_estacion" class="form__label">Nombre de la estacion</label>
-</div>
-<div class="input-field-fill margin-1">
-    <input type="input" class="form__field" placeholder=" " name="Kilometro_estacion" id="Kilometro_estacion"
-        required />
-    <label for="Kilometro_estacion" class="form__label">Kilometro de la estacion</label>
-</div>`;
-var id=0;
-
 let menu = document.getElementById("menu");
 menu.innerHTML = template;
 funcionalidad_menu();
 
-//iniciar botones
-document.getElementById("boton-less").addEventListener("click",()=>{delete_estaciones_lineas()});
-document.getElementById("boton-more").addEventListener("click",()=>{add_estaciones_lineas()})
-
-function add_estaciones_lineas() {
-    let contenedor = document.getElementById("contenedor-input-estaciones")
-    let div = document.createElement("div")
-    div.style="display: flex; width:100%"
-    div.id=id
-    id++;
-    div.innerHTML=template_add_estaciones;
-    contenedor.appendChild(div);
-}
-
-function delete_estaciones_lineas() {
-    if(id<=1){
-        console.log("entor")
-        return;
-    }
-    let contenedor = document.getElementById("contenedor-input-estaciones")
-    contenedor.removeChild(contenedor.lastChild)
-    id--;
-}
-add_estaciones_lineas()
