c71e27721cd7940a15a0dec55c0a086fda2e18cd
Felipe Urrutia <thewomins@gmail.com>
2022-06-25 15:14:32 -0400

    added date in search

--- src/views/user/scripts/buscar_recorrido.js
+++ src/views/user/scripts/buscar_recorrido.js
@@ -6,10 +6,28 @@ document.getElementById("search")
 
 search.addEventListener("click",e=>{onclick_search()})
 
+const datePicker = document.getElementById('fecha');
+
+
 async function onclick_search(){
     let origen = document.getElementById("Ciudad_origen")
     let destino = document.getElementById("Ciudad_destino")
-    if(origen.value === ""&& destino.value==""){return}
+    const selectedDate = new Date(datePicker.value);
+    const today = new Date();
+    today.setUTCHours(0,0,0,0);
+    if(origen.value === ""){
+        origen.setCustomValidity("No puede ser vacio");
+        origen.reportValidity();
+        return}
+    if(destino.value === ""){
+        destino.setCustomValidity("No puede ser vacio");
+        destino.reportValidity();
+        return}
+    if (isNaN(selectedDate) || selectedDate < today) {
+        datePicker.setCustomValidity("Ingrese una fecha correctamente")
+        datePicker.reportValidity();
+        return}
+    let fecha=[selectedDate.getDate()+1,(selectedDate.getMonth()+1),selectedDate.getFullYear()].join("-")
     let destino_estacion = (await get_estacion_by_city(destino.value)).nombre
     let array_final=[]
     let lista_origen= await get_linea_by_estacion( (await get_estacion_by_city(origen.value)).nombre)
@@ -18,5 +36,7 @@ async function onclick_search(){
         array_final.push(element);}
     });
     localStorage.setItem("lineas", JSON.stringify(array_final));
+    sessionStorage.setItem("fecha",fecha)
     window.location.href = "seleccion_horario.html?"+"origen="+origen.value+"&destino="+destino.value;
 }
+
